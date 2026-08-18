e653d2c138e340ac833881764511e783d0cb0f6f
gcaporncontracting-ops <3.07902086e+08+gcaporncontracting-ops@users.noreply.github.com>
2026-08-18 04:51:04 +0000

    Security: Update admin passcode to 94172079

--- src/worker.js
+++ src/worker.js
@@ -10,7 +10,7 @@ function slugify(name) {
 function uid() {
   return crypto.randomUUID();
 }
-var ADMIN_PASSCODE = "Warriors-Kick-9247";
+var ADMIN_PASSCODE = "94172079";
 var WEB3FORMS_ACCESS_KEY = "a59f79b9-cb63-4cc8-ab40-7465fd609f14";
 async function notifyAdminOfPinRequest(name) {
   try {
